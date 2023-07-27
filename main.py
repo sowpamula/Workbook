53822a6a2bd302ca3a5c2cda16895a725fa72f8a
sowjanya <sowjanyapamulapati55@gmail.com>
2023-07-27 12:51:13 -0700

    Add timr label feature

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from modules import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
+print("The time is below:")
+print("It is", now)
 
 while True:
     user_action = input("Type add, show, edit, complete or exit:")
